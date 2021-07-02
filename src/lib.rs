582126f54c201a18a5e16d004444b2ae3efed0be
espectro93 <gerdes.steffen@googlemail.com>
2021-07-02 20:25:57 +0200

    small refactoring but not stable rn

--- src/lib.rs
+++ src/lib.rs
@@ -9,44 +9,90 @@ use crate::crypt::{encrypt, decrypt};
 use std::fs;
 use image::{GenericImageView, ImageBuffer, Rgba, RgbImage, DynamicImage, Rgb, EncodableLayout, ImageFormat};
 
-
-fn from_file(mut file: File) -> Vec<u8> {
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer);
-    let bmp_header = &buffer[..54];
-    let mut encrypted_buffer = encrypt(buffer.as_slice());
-    let mut result_buffer = Vec::new();
-    result_buffer.extend_from_slice(&bmp_header);
-    result_buffer.append(&mut encrypted_buffer);
-    result_buffer
+enum ImageExtension {
+    PNG,
+    BMP,
+    JPG,
+    JPEG,
 }
 
-fn write_to_disk(data: Vec<u8>) {
-    let mut f = File::create("/home/steffen/CLionProjects/image-sec/resources/decrypted_image.bmp").expect("Unable to create file");
-    f.write_all(data.as_bytes()).expect("Unable to write data");
+struct EncryptedImage {
+    data: Vec<u8>
+}
+
+struct DecryptedImage {
+    data: Vec<u8>
+}
+
+pub trait FromFile<T> {
+    fn from_file(mut file: File) -> T;
+}
+
+pub trait WriteToDisk<T> {
+    fn write_to_disk(mut t: T) -> Vec<u8>;
+}
+
+impl FromFile<EncryptedImage> for EncryptedImage {
+    fn from_file(mut file: File) -> EncryptedImage {
+        unimplemented!()
+    }
+}
+
+impl FromFile<DecryptedImage> for DecryptedImage {
+    fn from_file(mut file: File) -> DecryptedImage {
+        unimplemented!()
+    }
+}
+
+impl WriteToDisk<EncryptedImage> for EncryptedImage {
+    fn write_to_disk(mut t: EncryptedImage) -> Vec<u8> {
+        unimplemented!()
+    }
+}
+
+impl WriteToDisk<DecryptedImage> for DecryptedImage {
+    fn write_to_disk(mut t: DecryptedImage) -> Vec<u8> {
+        unimplemented!()
+    }
+}
+
+//TODO: DEFINE FROM_FILE AS TRAIT
+impl EncryptedImage {
+    pub fn from_file(mut file: File) -> Self {
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer);
+        let bmp_header = &buffer[..54];
+        let mut encrypted_buffer = encrypt(buffer.as_slice());
+        let mut result_buffer = Vec::new();
+        result_buffer.extend_from_slice(&bmp_header);
+        result_buffer.append(&mut encrypted_buffer);
+        return EncryptedImage {
+            data: result_buffer
+        };
+    }
+}
+
+impl DecryptedImage {
+    pub fn from_file(mut file: File) -> Self {
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer);
+        let to_decrypt = &buffer[54..];
+        return DecryptedImage {
+            data: decrypt(to_decrypt)
+        };
+    }
 }
 
 pub fn encrypt_image(path: &Path) -> Result<(), Box<dyn Error>> {
     let (file, extension) = extract_file_with_extension(path)?;
     //TODO: CHECK EXTENSION AND PARSE TO BMP IF NECESSARY
-    write_to_disk(from_file(file));
+    write_to_disk(EncryptedImage::from_file(file));
     Ok(())
-}
-
-fn decrypt_from_file(mut file: File) -> Vec<u8>{
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer);
-    let to_decrypt = &buffer[54..];
-    decrypt(to_decrypt)
 }
 
 pub fn decrypt_image(path: &Path) -> Result<(), Box<dyn Error>> {
     let (file, extension) = extract_file_with_extension(path)?;
-    write_to_disk(decrypt_from_file(file));
-    Ok(())
-}
-
-pub fn open_image_and_decrypt(path: &Path) -> Result<(), Box<dyn Error>> {
+    write_to_disk(DecryptedImage::from_file(file));
     Ok(())
 }
 
@@ -61,4 +107,11 @@ fn extract_file_with_extension(path: &Path) -> Result<(File, &str), Box<dyn Erro
         .write(true)
         .open(path)?;
     Ok((file, file_extension))
+}
+
+//TODO: DO THIS OVER A TRAIT THAT IS PASSED IN AND DELIVERS DATA
+fn write_to_disk(data: Vec<u8>) {
+    //TODO: DETERMINE CURRENT DIRECTORY AND WRITE TO THAT
+    let mut f = File::create("/home/steffen/CLionProjects/image-sec/resources/decrypted_image.bmp").expect("Unable to create file");
+    f.write_all(data.as_bytes()).expect("Unable to write data");
 }
